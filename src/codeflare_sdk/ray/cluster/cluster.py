93bd6ef3531536d016e4ed62924948c41636e592
kryanbeane <bryankeane0@gmail.com>
2026-01-22 11:33:13 +0000

    RHAIENG-2063: Add new RayCluster object

--- src/codeflare_sdk/ray/cluster/cluster.py
+++ src/codeflare_sdk/ray/cluster/cluster.py
@@ -39,7 +39,7 @@ from ...common import _kube_api_error_handling
 from .config import ClusterConfiguration
 from .status import (
     CodeFlareClusterStatus,
-    RayCluster,
+    RayClusterInfo,
     RayClusterStatus,
 )
 from ...common.widgets.widgets import (
@@ -511,7 +511,7 @@ class Cluster:
             sleep(5)
             time += 5
 
-    def details(self, print_to_console: bool = True) -> RayCluster:
+    def details(self, print_to_console: bool = True) -> RayClusterInfo:
         """
         Retrieves details about the Ray Cluster.
 
@@ -524,7 +524,7 @@ class Cluster:
                 printed to the console. Defaults to True.
 
         Returns:
-            RayCluster:
+            RayClusterInfo:
                 A copy of the Ray Cluster details.
         """
         cluster = _copy_to_ray(self)
@@ -990,7 +990,7 @@ def _get_ingress_domain(self):  # pragma: no cover
     return domain
 
 
-def _ray_cluster_status(name, namespace="default") -> Optional[RayCluster]:
+def _ray_cluster_status(name, namespace="default") -> Optional[RayClusterInfo]:
     try:
         config_check()
         api_instance = client.CustomObjectsApi(get_api_client())
@@ -1011,7 +1011,7 @@ def _ray_cluster_status(name, namespace="default") -> Optional[RayCluster]:
 
 def _get_ray_clusters(
     namespace="default", filter: Optional[List[RayClusterStatus]] = None
-) -> List[RayCluster]:
+) -> List[RayClusterInfo]:
     list_of_clusters = []
     try:
         config_check()
@@ -1037,7 +1037,7 @@ def _get_ray_clusters(
     return list_of_clusters
 
 
-def _map_to_ray_cluster(rc) -> Optional[RayCluster]:
+def _map_to_ray_cluster(rc) -> Optional[RayClusterInfo]:
     if "status" in rc and "state" in rc["status"]:
         status = RayClusterStatus(rc["status"]["state"].lower())
     else:
@@ -1095,7 +1095,7 @@ def _map_to_ray_cluster(rc) -> Optional[RayCluster]:
         worker_extended_resources,
     ) = Cluster._head_worker_extended_resources_from_rc_dict(rc)
 
-    return RayCluster(
+    return RayClusterInfo(
         name=rc["metadata"]["name"],
         status=status,
         # for now we are not using autoscaling so same replicas is fine
@@ -1131,8 +1131,8 @@ def _map_to_ray_cluster(rc) -> Optional[RayCluster]:
     )
 
 
-def _copy_to_ray(cluster: Cluster) -> RayCluster:
-    ray = RayCluster(
+def _copy_to_ray(cluster: Cluster) -> RayClusterInfo:
+    ray = RayClusterInfo(
         name=cluster.config.name,
         status=cluster.status(print_to_console=False)[0],
         num_workers=cluster.config.num_workers,
